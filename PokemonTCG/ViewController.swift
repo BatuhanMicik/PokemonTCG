56d30084283b0a4516124d6579148eca0d9dd298
batuhanmicik <necdet.micik@isik.edu.tr>
2021-08-06 17:58:45 +0300

    Initial Commit

---
+++ PokemonTCG/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  PokemonTCG
+//
+//  Created by Batuhan Mıcık on 6.08.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
